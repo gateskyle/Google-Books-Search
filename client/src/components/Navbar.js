2414f883c77ac4ab41204f95af2a10e6b98884bb
Kyle G <legokyle@frontier.com>
2021-04-14 14:33:21 -0700

    Inital push of Navbar (No working links yet)

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar, Nav } from 'react-bootstrap';
+
+function NavBar() {
+
+    return(
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" >
+            <Navbar.Brand><h1>Google Books Search System</h1></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                    <Nav.Link href="#/"><h3>Search For Books</h3></Nav.Link>
+                    <Nav.Link href="#portfolio"><h3>View Saved Books</h3></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+    
+    export default NavBar;
