d3dfa3dd05e2870af8a9c08e935069385a0ee3f3
Kyle G <legokyle@frontier.com>
2021-04-14 17:03:45 -0700

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from './components/Navbar'
 import HeaderSearchBar from './components/HeaderSearchBar'
+import ResultsContainer from './components/ResultContainers'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap'
 import "./App.css";
@@ -10,6 +11,7 @@ function App() {
     <>
       <NavBar />
         <HeaderSearchBar />
+        <ResultsContainer />
     </>
   );
 }
