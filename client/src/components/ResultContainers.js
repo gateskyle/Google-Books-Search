aec72bcaf8942adfa1ed4e75b2e03a71ce9dcdee
Kyle G <legokyle@frontier.com>
2021-04-15 23:34:52 -0700

    Updated Resultscontainer

--- client/src/components/ResultContainers.js
+++ client/src/components/ResultContainers.js
@@ -1,15 +1,31 @@
 import React from "react";
+import API from '../utils/API'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './ResultContainer.css'
     
 function ResultContainers(props) {  
+
+    function clickID() {
+        const newBook = {
+            id: props.id,
+            title: props.title, 
+            authors: props.authors, 
+            desc: props.desc,
+            pic: props.pic, 
+            link: props.link
+        }
+        console.log(newBook);
+        API.save(newBook);
+    }
+
+
     return (
         <>
         <div>
         <section className="container">
             <section className="row flex-row-reverse p-2">
                 <section>
-                    <input className="btn btn-primary" type="submit" value="Save Book"></input>
+                    <input className="btn btn-primary" type="submit" value="Save Book" id={props.id} onClick={clickID}></input>
                     <a href={props.link} className="btn btn-primary view" role="button">View Book</a>
                 </section>
             </section>
@@ -17,7 +33,7 @@ function ResultContainers(props) {
             <section className="row">
                 <section className="col-sm">
                     <h1 className="font-weight-bold">Title: {props.title}</h1>
-                    <h2>Author(s): {props.author}</h2>
+                    <h2>Author(s): {props.authors}</h2>
                     <hr/>
                 </section>
             </section>
