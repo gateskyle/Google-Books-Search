78852197c056cccea1bee3bc6853b73ec54bae64
Kyle G <legokyle@frontier.com>
2021-04-15 23:36:19 -0700

    updated API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,43 @@ import axios from "axios";
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export default {
+
   search: function(query) {
     return axios.get(BASEURL + query);
+  },
+
+  save: function (data) {
+    return axios.post("/api/books", {
+      id: data.id,
+      title: data.title, 
+      authors: data.authors, 
+      desc: data.desc,
+      pic: data.pic, 
+      link: data.link
+    })
+    .then(function (response) {
+      console.log(response);
+      alert('Book Has Been Saved')
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  },
+
+  getAllBooks: function () {
+    return axios.get('/api/books')
+  },
+
+  deleteBook: function (id) {
+    console.log('id is', id);
+    return axios
+    .delete(`/api/books/${id}`)
+    .then( function (response) {
+      console.log('deleted', response)
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
   }
+
 };
