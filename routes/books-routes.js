a0b76e761eec5762a75602f189b265773d823ee4
Kyle G <legokyle@frontier.com>
2021-04-15 17:55:27 -0700

    Added routes folder

---
+++ routes/books-routes.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Books = require("../models/books.js");
+
+router.get('/api/books', async (req, res) => {
+  Books.find({}).then(
+    (data) => {
+      res.json(data);
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+});
+
+router.post('/api/books', async (req, res) => {
+  try {
+    const book = await Books.create({
+      title: req.body.title,
+      authors: req.body.authors,
+      desc: req.body.description,
+      image: req.body.image,
+      link: req.body.link,
+    })
+    res.json(book);
+    }
+    catch (err) {
+      console.log(err);
+    }
+});
+
+router.delete('/api/books/:id', async (req, res) => {
+  Books.findOneAndRemove({ _id: req.params.id }, req.body, function(err, data)
+  {
+      if(!err){
+        console.log("Book Deleted");
+      } 
+      else {
+        console.log(err)
+      }
+  });
+})   
+
+module.exports = router;
