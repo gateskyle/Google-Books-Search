f67bffc647eb9fe9474463a6010fddc92cbead35
Kyle G <legokyle@frontier.com>
2021-04-15 23:41:48 -0700

    fixed Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,8 +9,8 @@ function NavBar() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                    <Nav.Link href="#/"><h4>Search For Books</h4></Nav.Link>
-                    <Nav.Link href="#saved"><h4>View Saved Books</h4></Nav.Link>
+                    <Nav.Link href="/"><h4>Search For Books</h4></Nav.Link>
+                    <Nav.Link href="saved"><h4>View Saved Books</h4></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
